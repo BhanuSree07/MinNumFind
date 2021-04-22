dea7099e2594095e18abd113eaeeb03aac37e969
BhanuSree07 <81431701+BhanuSree07@users.noreply.github.com>
2021-04-22 19:08:45 +0530

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -14,12 +14,21 @@ public class Test1
 	}
 	public static void main(String []args)
 	{
-	   ArrayList<Integer> list = new ArrayList<Integer>();
-      	   list.add(800);
-      	   list.add(5500);
-      	   list.add(6700);
-      	   list.add(9990);
-      	   System.out.println("Original list : " + list);
+	  // ArrayList<Integer> list = new ArrayList<Integer>();
+      	  // list.add(800);
+      	  // list.add(5500);
+      	  // list.add(6700);
+      	  // list.add(9990);
+      	  // System.out.println("Original list : " + list);
+	   String v1 ="INR6700";
+	   String v2="INR5500";
+	   String v3="INR800";
+	   String v4="INR9990";
+	   System.out.println(v1);
+	   System.out.println(v2);
+	   System.out.println(v3);
+	   System.out.println(v4);
+
 	   int array[] = new int[]{800,5500,6700,9990};
  	   int min = getMin(array);
            System.out.println("Minimum Value is: "+min);
